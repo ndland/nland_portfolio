aaa4201c089f55e8fa933ea2b540ac641f68dff4
Nick Land <land.d.nicholas@gmail.com>
2024-01-15 15:34:13 -0500

    chore(storybook): add pre-defined viewports to Storybook
    This provides a lot of pre-defined viewports so I can test my app on multiple screen sizes.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from "@storybook/react";
+import {
+  INITIAL_VIEWPORTS,
+  MINIMAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
 import "../src/index.css";
 
 const preview: Preview = {
@@ -9,6 +13,13 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS,
+      },
+      defaultViewport: "iphone13promax",
     },
   },
 };
