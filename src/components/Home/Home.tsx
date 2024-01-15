113793d4706b77970ce3c1021ff7ac88c6ff96fb
Nick Land <land.d.nicholas@gmail.com>
2024-01-15 14:09:00 -0500

    style(home): reorder class names
    Prettier plugin ran, and class names were reordered.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC<HomeProps> = () => {
     <>
       <div className="flex h-screen items-center justify-center bg-neutral-600">
         <div className="absolute right-8 top-8 text-green-500">
-          <div className="flex space-x-8 font-roboto">
+          <div className="font-roboto flex space-x-8">
             <a href="">Home</a>
             <a href="">About</a>
             <a href="">Projects</a>
           </div>
         </div>
-        <div className="flex items-center justify-center font-roboto-slab text-8xl font-bold text-white">
+        <div className="font-roboto-slab flex items-center justify-center text-8xl font-bold text-white">
           <div className="flex flex-col items-center space-y-8">
             <div>Hello! I'm Nick</div>
             <div className="text-3xl">I build for the web</div>
