8af6ec281d442ded81a85855f158f7c7a467916c
Nick Land <land.d.nicholas@gmail.com>
2024-01-15 15:49:26 -0500

    feat(storybook): add ability to toggle dark mode in Storybook
    This is really helpful for allowing for a dark mode to my portfolio, but also, still developing for
    a light view as well.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,6 +6,11 @@ import {
 import "../src/index.css";
 
 const preview: Preview = {
+  globalTypes: {
+    darkMode: {
+      defaultValue: true, // Enable dark mode by default on all stories
+    },
+  },
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,18 +5,18 @@ interface HomeProps {}
 const Home: React.FC<HomeProps> = () => {
   return (
     <>
-      <div className="flex h-screen items-center justify-center bg-neutral-600">
+      <div className="flex h-screen items-center justify-center dark:bg-neutral-600">
         <div className="absolute right-8 top-8 text-green-500">
-          <div className="font-roboto flex space-x-8">
+          <div className="flex space-x-8 font-roboto">
             <a href="">Home</a>
             <a href="">About</a>
             <a href="">Projects</a>
           </div>
         </div>
-        <div className="font-roboto-slab flex items-center justify-center text-8xl font-bold text-white">
+        <div className="flex items-center justify-center font-roboto-slab text-4xl font-bold text-orange-500 dark:text-white md:text-8xl">
           <div className="flex flex-col items-center space-y-8">
             <div>Hello! I'm Nick</div>
-            <div className="text-3xl">I build for the web</div>
+            <div className="text-xl md:text-3xl">I build for the web</div>
           </div>
         </div>
       </div>
