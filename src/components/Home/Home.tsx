c2f3b4f881057ad3b313a9b295302edd19ab7688
Nick Land <land.d.nicholas@gmail.com>
2024-01-15 19:32:33 -0500

    feat(flowbite): add Flowbite to the project
    I've added Flowbite as it is a minimal library that will provide some helpful components for me to
    use. It is still compatible with Tailwind, so that is nice.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,18 +5,22 @@ interface HomeProps {}
 const Home: React.FC<HomeProps> = () => {
   return (
     <>
-      <div className="flex h-screen items-center justify-center dark:bg-neutral-600">
-        <div className="absolute right-8 top-8 text-green-500">
-          <div className="flex space-x-8 font-roboto">
+      <div className="flex h-screen items-center justify-center dark:bg-gray-500">
+        <div className="absolute right-9 top-9 text-green-400">
+          <div className="invisible flex space-x-8 md:visible md:text-2xl">
             <a href="">Home</a>
             <a href="">About</a>
             <a href="">Projects</a>
           </div>
         </div>
-        <div className="flex items-center justify-center font-roboto-slab text-4xl font-bold text-orange-500 dark:text-white md:text-8xl">
-          <div className="flex flex-col items-center space-y-8">
-            <div>Hello! I'm Nick</div>
-            <div className="text-xl md:text-3xl">I build for the web</div>
+        <div className="flex items-center justify-center font-bold text-gray-500 dark:text-white md:text-8xl">
+          <div className="flex flex-col items-center space-y-9">
+            <div className="text-4xl font-semibold md:text-8xl">
+              Hello! I'm Nick
+            </div>
+            <div className="text-2xl font-light md:text-4xl md:font-normal">
+              I build for the web
+            </div>
           </div>
         </div>
       </div>
