0798a4b826ef01348b7b3839502f0954f0656d4c
Nick Land <land.d.nicholas@gmail.com>
2024-01-23 17:07:39 -0500

    feat(app): add <Home /> & <About /> to the main app
    There is a click handler that gets passed into the components, this will allow for navigating
    between screeens.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import About from "./components/About/About.tsx";
 import Home from "./components/Home/Home.tsx";
 
+const handleClick = () => {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
+};
+
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home onClick={handleClick} />
+      <About />
+    </>
+  );
 }
 
 export default App;
